30aa1e9f4e8aee75a9efa1f934a4f468ccafb325
alexsavinov <spell477@gmail.com>
2023-05-03 20:41:00 +0300

    first commit

---
+++ src/main/java/com/epam/esm/epammodule4/EpamModule4Application.java
@@ -0,0 +1,13 @@
+package com.epam.esm.epammodule4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamModule4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpamModule4Application.class, args);
+	}
+
+}
