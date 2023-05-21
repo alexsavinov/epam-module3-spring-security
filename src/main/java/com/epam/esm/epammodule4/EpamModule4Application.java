96c596251a0749f4a5bbd1a92f7e1f5b6d8ca453
alexsavinov <spell477@gmail.com>
2023-05-21 10:19:06 +0300

    tomcat

--- src/main/java/com/epam/esm/epammodule4/EpamModule4Application.java
+++ src/main/java/com/epam/esm/epammodule4/EpamModule4Application.java
@@ -2,10 +2,11 @@ package com.epam.esm.epammodule4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class EpamModule4Application {
+public class EpamModule4Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(EpamModule4Application.class, args);
